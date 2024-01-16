f5f65d0226e8ee14f7a88ebcbf71361a02646469
Joris Machielse <8628+joris@users.noreply.github.com>
2024-01-16 15:55:59 +0100

    Update to working version

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-WARNING: This is a DRAFT project, and has not been built yet.
-
 # azure-sign-tool-electron-forge-plugin
 
 I wanted to sign the Windows app that we’re building (www.recordonce.com) with an EV certificate in a Github Actions build pipeline.
 
 [This article provided](https://melatonin.dev/blog/how-to-code-sign-windows-installers-with-an-ev-cert-on-github-actions/) 99% of the solution. But as Electron Forge does not use AzureCodeSign which is necessary to work with HSM and Azure Key Vault, I adapted another plugin ([@burzo/electron-forge-ssl-code-sign-plugin](https://github.com/Burzo/electron-forge-ssl-code-sign-plugin/)) to hopefully fix that.
 
-The code is heavily based on [@burzo/electron-forge-ssl-code-sign-plugin](https://github.com/Burzo/electron-forge-ssl-code-sign-plugin/).
+The code is inspired by [@burzo/electron-forge-ssl-code-sign-plugin](https://github.com/Burzo/electron-forge-ssl-code-sign-plugin/), and originally heavily borrowed from that project.
 
 ## Prerequisites
 
@@ -14,7 +12,7 @@ This plugin works with electron-forge version >=7.
 
 Additionally, you need to install the [AzureSignTool](https://github.com/vcsjones/AzureSignTool).
 
-This plugin currently only supports building on Windows-based machines.
+This plugin only supports building on Windows-based machines. That’s because both Squirrel and AzureSignTool only work on Windows.
 
 ## Installation
 
@@ -33,23 +31,94 @@ yarn add --dev azure-sign-tool-electron-forge-plugin
 The plugin accepts the configuration variables that are used by this guide on how to sign code with an EV certificate.
 The variables correspond to [AzureCodeSign’s paramaters](https://github.com/vcsjones/AzureSignTool#parameters).
 
-Include the plugin in your Forge config as follows:
+### Make sure you make with Squirrel:
+
+forge.config.ts:
 
 ```
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+
   ...,
-  "plugins": [
-    {
-      name: "azure-sign-tool-electron-forge-plugin",
-      config: {
-        azureKeyVaultUri: "",
-        azureClientId: "",
-        azureTenantId: "",
-        azureClientSecret: "",
-        azureCertificateName: "",
-      },
-    },
+  makers: [
+    new MakerSquirrel((arch) => ({
+```
+
+## Include the plugin in your Forge config as follows:
+
+forge.config.ts:
+
+```
+import { ElectronForgeAzureSignToolPlugin } from "azure-sign-tool-electron-forge-plugin";
+
+const config: ForgeConfig = {
+  ...,
+  plugins: [
+    // Make sure you
+    new ElectronForgeAzureSignToolPlugin({
+      azureKeyVaultUri: process.env.AZURE_KEY_VAULT_URI || "",
+      azureClientId: process.env.AZURE_CLIENT_ID || "",
+      azureTenantId: process.env.AZURE_TENANT_ID || "",
+      azureClientSecret: process.env.AZURE_CLIENT_SECRET || "",
+      azureCertificateName: process.env.AZURE_CERTIFICATE_NAME || "",
+    }),
   ],
   ...,
+```
+
+### Your Github Actions workflow should look something like this:
+
+```
+# taken from https://github.com/electron/fiddle/blob/main/.github/workflows/build.yaml
+name: Build & Release
+
+on:
+  push:
+    branches:
+      - master
+    tags:
+      - v*
+  pull_request:
+
+env:
+  NPM_REGISTRY: npm.pkg.github.com
+
+jobs:
+  build:
+    if: startsWith(github.ref, 'refs/tags/')
+    name: Build (${{ matrix.os }} - ${{ matrix.arch }})
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        # Build for supported platforms
+        # https://github.com/electron/electron-packager/blob/ebcbd439ff3e0f6f92fa880ff28a8670a9bcf2ab/src/targets.js#L9
+        # 32-bit Linux unsupported as of 2019: https://www.electronjs.org/blog/linux-32bit-support
+        os: [macos-latest, ubuntu-latest, windows-latest]
+        arch: [x64]
+        include:
+          - os: macos-latest
+            arch: universal
+          - os: macos-latest
+            arch: arm64
+          - os: windows-latest
+            arch: ia32
+
+    steps:
+      […]
+
+      - name: Install AzureSignTool on Windows
+        if: matrix.os == 'windows-latest'
+        shell: bash
+        run: dotnet tool install --global AzureSignTool
+
+      - name: Make & Publish
+        if: startsWith(github.ref, 'refs/tags/')
+        run: yarn electron-forge publish --arch=${{ matrix.arch }}
+        env:
+          AZURE_KEY_VAULT_URI: ${{ secrets.AZURE_KEY_VAULT_URI }}
+          AZURE_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+          AZURE_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+          AZURE_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+          AZURE_CERTIFICATE_NAME: ${{ secrets.AZURE_CERTIFICATE_NAME }}
 ```
 
 ## Contribution
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import {
   ResolvedForgeConfig,
 } from "@electron-forge/shared-types";
 import { execSync, ExecSyncOptionsWithStringEncoding } from "child_process";
-import fs from "fs-extra";
 
 type ConfigTypes = {
   azureKeyVaultUri: string;
@@ -14,7 +13,6 @@ type ConfigTypes = {
   azureTenantId: string;
   azureClientSecret: string;
   azureCertificateName: string;
-  // signToolPath: string
 };
 
 export class ElectronForgeAzureSignToolPlugin extends PluginBase<ConfigTypes> {
@@ -23,6 +21,24 @@ export class ElectronForgeAzureSignToolPlugin extends PluginBase<ConfigTypes> {
   constructor(config: ConfigTypes) {
     super(config);
     this.config = config;
+    console.log(`===== loading ${this.name} =====`);
+    console.log(JSON.stringify(config, null, 2));
+
+    if (
+      !config.azureKeyVaultUri ||
+      !config.azureClientId ||
+      !config.azureTenantId ||
+      !config.azureClientSecret ||
+      !config.azureCertificateName
+    ) {
+      throw new Error(
+        `You did not provide all the required config variables to ${
+          this.name
+        }.\nCurrent values:\n${Object.keys(config)
+          .map((key) => `${key}: ${config[key as keyof ConfigTypes]}`)
+          .join("\n")}`
+      );
+    }
   }
 
   getHooks(): ForgeMultiHookMap {
@@ -33,26 +49,8 @@ export class ElectronForgeAzureSignToolPlugin extends PluginBase<ConfigTypes> {
     forgeConfig: ResolvedForgeConfig,
     makeResults: ForgeMakeResult[]
   ) => {
-    const squirrelMaker: IForgeResolvableMaker = forgeConfig.makers.find(
-      (maker) =>
-        (maker as IForgeResolvableMaker)?.name ===
-        "@electron-forge/maker-squirrel"
-    ) as IForgeResolvableMaker;
-
-    if (!squirrelMaker) {
-      throw new Error(
-        `The plugin ${this.name} can not work without @electron-forge/maker-squirrel. Remove it from the plugins array.`
-      );
-    }
-
-    const {
-      azureKeyVaultUri,
-      azureClientId,
-      azureTenantId,
-      azureClientSecret,
-      azureCertificateName,
-      // signToolPath
-    } = this.config;
+    // console.log(`===== ${this.name}.postMake =====`);
+    // console.log(JSON.stringify({ forgeConfig, makeResults }, null, 2));
 
     return makeResults.map((data) => {
       const { artifacts, platform } = data;
@@ -61,82 +59,61 @@ export class ElectronForgeAzureSignToolPlugin extends PluginBase<ConfigTypes> {
         return data;
       }
 
-      if (
-        !azureKeyVaultUri ||
-        !azureClientId ||
-        !azureTenantId ||
-        !azureClientSecret ||
-        !azureCertificateName
-        // !signToolPath
-      ) {
-        throw new Error(
-          `You did not provide all the required config variables to ${
-            this.name
-          }.\nCurrent values:\n${Object.keys(this.config)
-            .map((key) => `${key}  -  ${this.config[key as keyof ConfigTypes]}`)
-            .join("\n")}`
-        );
-      }
-
-      const releasesPath = artifacts[0];
-
-      /**
-       * The exe is located where RELEASES is.
-       */
-      if (releasesPath.endsWith("RELEASES")) {
-        /**
-         * We first parse the RELEASES file to get out the .nupkg file name
-         * saved there. If setupExe wasn't changed in the squirrel maker
-         * config, then we can use this parsed name to get the .exe file
-         * name as well.
-         *
-         * If setupExe was set, then we need to use that instead to get
-         * the correct .exe path for signing.
-         */
-        const nupkgFileName = fs
-          .readFileSync(releasesPath, "utf8")
-          .split(" ")[1];
-
-        const exeName = squirrelMaker.config?.setupExe
-          ? squirrelMaker.config?.setupExe
-          : nupkgFileName.replace("-full.nupkg", ".exe");
-
-        const exeFilePath = releasesPath.replace("RELEASES", exeName);
-
-        const execSyncSettings = {
-          stdio: "inherit",
-          encoding: "utf8",
-          // env: {
-          // 	...process.env,
-          // },
-        } as ExecSyncOptionsWithStringEncoding;
-
-        /**
-         * We sign all the .exe files?
-         */
-        try {
-          execSync(
-            `AzureSignTool sign \
-              --azure-key-vault-url "${azureKeyVaultUri}" \
-              --azure-key-vault-client-id "${azureClientId}" \
-              --azure-key-vault-tenant-id "${azureTenantId}" \
-              --azure-key-vault-client-secret "${azureClientSecret}" \
-              --azure-key-vault-certificate ${azureCertificateName} \
-              --timestamp-rfc3161 http://timestamp.digicert.com \
-              --verbose \
-              ${exeFilePath}`,
-            execSyncSettings
-          );
-        } catch (e) {
-          if (e instanceof Error) {
-            throw new Error(e.message);
-          } else {
-            throw e;
-          }
+      // Example "artifacts": [
+      //   "D:\\a\\desktop\\desktop\\out\\beta\\make\\squirrel.windows\\ia32\\RELEASES",
+      //   "D:\\a\\desktop\\desktop\\out\\beta\\make\\squirrel.windows\\ia32\\appname-4.2.18-beta-win32-ia32-beta-setup.exe",
+      //   "D:\\a\\desktop\\desktop\\out\\beta\\make\\squirrel.windows\\ia32\\appname-4.2.18-beta-full.nupkg"
+      // ],
+      artifacts.forEach((artifactPath) => {
+        if (artifactPath.endsWith(".exe")) {
+          this.sign(artifactPath, this.config);
         }
-      }
+      });
 
       return data;
     });
   };
+
+  sign(path: string, config: ConfigTypes): void {
+    const execSyncSettings = {
+      stdio: "inherit",
+      encoding: "utf8",
+      // env: {
+      // 	...process.env,
+      // },
+    } as ExecSyncOptionsWithStringEncoding;
+
+    const {
+      azureKeyVaultUri,
+      azureClientId,
+      azureTenantId,
+      azureClientSecret,
+      azureCertificateName,
+    } = this.config;
+
+    console.log("Signing: " + path);
+
+    try {
+      execSync(
+        `AzureSignTool sign \
+                --azure-key-vault-url "${azureKeyVaultUri}" \
+                --azure-key-vault-client-id "${azureClientId}" \
+                --azure-key-vault-tenant-id "${azureTenantId}" \
+                --azure-key-vault-client-secret "${azureClientSecret}" \
+                --azure-key-vault-certificate ${azureCertificateName} \
+                --timestamp-rfc3161 http://timestamp.digicert.com \
+                --verbose \
+                ${path}`,
+        execSyncSettings
+      );
+    } catch (e) {
+      console.log("Error in signFile");
+      if (e instanceof Error) {
+        console.log(e.message);
+        throw new Error(e.message);
+      } else {
+        throw e;
+      }
+    }
+  }
 }
